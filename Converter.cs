3c8371ba435660ad02ad111c96bc9396ee8d707c
vladaaagl <vsh_o@ukr.net>
2022-09-25 16:10:15 +0300

    Second task to the second lab

---
+++ Converter.cs
@@ -0,0 +1,6 @@
+class Converter
+{
+    public double uah;
+    public double eur;
+    public double usd;
+}
---
+++ Program.cs
@@ -0,0 +1,75 @@
+﻿Converter converter = new Converter();
+
+Console.Write("Вкажіть яку саме валюту бажаєте конвертувати (якщо гривню - 1, євро - 2, долар - 3): ");
+string? value = Console.ReadLine();
+
+Console.Write("В яку валюту потрібно конвертувати (якщо в гривню - 1, євро - 2, долар - 3): ");
+string? converted = Console.ReadLine();
+double result;
+
+#region Converter
+if (converted == "1" && value == "1")
+{
+    Console.Write("Ви бажаєте конвертувати однакові валюти. Змініть одну з валют.");
+    return;
+}
+else if (converted == "2" && value == "1")
+{
+    Console.Write("Введіть суму, яку потрібно конвертувати: ");
+    converter.uah = Convert.ToDouble(Console.ReadLine());
+
+    result = converter.uah * 0.0244;
+}
+else if (converted == "3" && value == "1")
+{
+    Console.Write("Введіть суму, яку потрібно конвертувати: ");
+    converter.uah = Convert.ToDouble(Console.ReadLine());
+
+    result = converter.uah * 0.0238;
+}
+else if (converted == "1" && value == "2")
+{
+    Console.Write("Введіть суму, яку потрібно конвертувати: ");
+    converter.eur = Convert.ToDouble(Console.ReadLine());
+
+    result = converter.eur * 40.95;
+}
+else if (converted == "2" && value == "2")
+{
+    Console.Write("Ви бажаєте конвертувати однакові валюти. Змініть одну з валют.");
+    return;
+}
+else if (converted == "3" && value == "2")
+{
+    Console.Write("Введіть суму, яку потрібно конвертувати: ");
+    converter.eur = Convert.ToDouble(Console.ReadLine());
+
+    result = converter.eur * 0.95;
+}
+else if (converted == "1" && value == "3")
+{
+    Console.Write("Введіть суму, яку потрібно конвертувати: ");
+    converter.usd = Convert.ToDouble(Console.ReadLine());
+
+    result = converter.usd * 42;
+}
+else if (converted == "2" && value == "3")
+{
+    Console.Write("Введіть суму, яку потрібно конвертувати: ");
+    converter.usd = Convert.ToDouble(Console.ReadLine());
+
+    result = converter.usd * 0.9953;
+}
+else if (converted == "3" && value == "3")
+{
+    Console.Write("Ви бажаєте конвертувати однакові валюти. Змініть одну з валют.");
+    return;
+}
+else
+{
+    Console.WriteLine("Невірно вказанй код валюти.");
+    return;
+}
+#endregion
+
+Console.WriteLine("Конвертована сума: " + result);
